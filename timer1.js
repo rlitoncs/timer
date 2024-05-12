1c776bd9899bae658e33a89db68a2abbc1807c1c
rlitoncs <rj_liton@hotmail.com>
2024-05-12 19:28:20 -0400

    Eslint file

--- timer1.js
+++ timer1.js
@@ -1,22 +1,22 @@
 
-const input = process.argv
+const input = process.argv;
 
 //Edge Case 1: No numbers provided:
-if (input.length === 2){
-  console.log( "No Alarm Added");
-} 
+if (input.length === 2) {
+  console.log("No Alarm Added");
+}
 
 const setAlarm = (input) => {
 
-for (let i = 2; i < input.length; i++){
+  for (let i = 2; i < input.length; i++) {
   //Edge Case 2 & 3: Input Has to be a Number and greater or equal to 0
-  if (Number(input[i]) && Number(input[i]) >= 0){
-    setTimeout(() => {
-      process.stdout.write("."); //switched to "." because beep is not working
-    }, Number(input[i]) * 1000)
+    if (Number(input[i]) && Number(input[i]) >= 0) {
+      setTimeout(() => {
+        process.stdout.write("."); //switched to "." because beep is not working
+      }, Number(input[i]) * 1000);
+    }
   }
-}
 
-}
+};
 
 setAlarm(input);
